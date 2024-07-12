4cf5c0f3c90d0bf23c612f3c5c4da01f4b3f8742
awais-ahmad01 <aahmad19376@gmail.com>
2024-07-12 09:50:58 -0700

    Project Completed

--- src/components/stage-2.js
+++ src/components/stage-2.js
@@ -1,11 +1,34 @@
 
+import React, { useContext } from "react";
+
+import { MyContext } from "../context";
 
 
 const Stage2= ()=> {
 
+    
+    const context = useContext(MyContext);
+
     return(
         <>
-            Stage2
+            <div className="result_wrapper">
+                <h3>The Looser is:</h3>
+
+                <div>
+                    {context.state.result}
+                </div>
+            </div>
+
+            <div className="action_button"
+                onClick={()=> context.resetGame()}>
+                    Start Over
+            </div>
+
+            <div className="action_button btn_2"
+                onClick={()=> context.getNewLooser()}>
+                    Get new Looser
+            </div>
+        
         </>
     )
 }
